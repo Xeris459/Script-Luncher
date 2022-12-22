518865a793ba7f302dbb87ed815e264a275ef501
Vikri Anto <30387870+Xeris459@users.noreply.github.com>
2022-12-22 19:10:36 +0800

    remove vue-simpple-context-menu component

--- src/js/main/main.ts
+++ src/js/main/main.ts
@@ -1,10 +1,9 @@
 import { createApp } from "vue";
 import { createPinia } from 'pinia'
-import VueSimpleContextMenu from 'vue-simple-context-menu';
 import 'vue-simple-context-menu/dist/vue-simple-context-menu.css';
 
 import App from "./main.vue";
 
 const pinia = createPinia()
 
-createApp(App).use(pinia).component('vue-simple-context-menu', VueSimpleContextMenu).mount("#root")
+createApp(App).use(pinia).mount("#root")
